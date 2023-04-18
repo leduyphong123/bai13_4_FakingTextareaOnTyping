18f838d8b9c72b9f9c5df7929a52010c8d509ab5
leduyphong123 <leduyphongck789@gmail.com>
2023-04-18 11:07:19 +0700

    finnish

---
+++ index.js
@@ -0,0 +1,15 @@
+let value=document.getElementById("value");
+value.addEventListener("input",changeValue);
+let texValue="Anh Tiến Bê ĐÊ";
+let count=0;
+
+function changeValue(evt){
+    let a=evt;
+    value.value =value.value.substring(0,value.value.length-1)+ texValue.charAt(count);
+    if(count>texValue.length){
+        count=0;
+        value.value="";
+    }else{
+        count++;
+    }
+}
